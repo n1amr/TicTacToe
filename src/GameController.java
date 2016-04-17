297ca799c0b900c7c455f08ab5fa4026deaaa429
Amr Alaa <n1amr1@gmail.com>
2016-04-17 17:30:49 +0200

    separate controller

---
+++ src/GameController.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class GameController {
+
+	public void run() {
+		Scanner in = new Scanner(System.in);
+		System.out.println("Hello");
+
+		Board board = new Board();
+
+		char player;
+		System.out.println(board);
+		for (int game = 0; !board.gameFinished(); game++) {
+			player = (game % 2 == 0) ? Board.X_PLAYER : Board.O_PLAYER;
+			if (player == Board.X_PLAYER) {
+				int i = in.nextInt() - 1;
+				int j = in.nextInt() - 1;
+				board.play(i, j, player);
+			} else {
+				board = board.smartPlay(board.O_PLAYER);
+			}
+			System.out.println(board);
+		}
+
+		if (board.getStatus() == Board.X_WINS)
+			System.out.println("X wins");
+		else if (board.getStatus() == Board.O_WINS)
+			System.out.println("O wins");
+		else if (board.getStatus() == Board.DRAW)
+			System.out.println("Draw");
+	}
+}
