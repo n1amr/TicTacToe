1ea5c7a9f9680908b8fb9324668724b3bd217c8c
Amr Alaa <n1amr1@gmail.com>
2016-04-20 16:26:34 +0200

    add comment

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import javax.swing.SwingUtilities;
 public class Main {
 
 	public static void main(String[] args) {
+		/** Run the GUI form */
 		SwingUtilities.invokeLater(new Runnable() {
 			@Override
 			public void run() {
